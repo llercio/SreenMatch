ea0fc4abbc99dbc9d1ad2979b37380d86bcf9311
llercio <lercioluiz1995@gmail.com>
2025-12-26 20:14:49 -0300

    adding ScreenMatch to its repository

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("The Godfather");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
